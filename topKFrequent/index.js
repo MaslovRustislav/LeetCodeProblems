f39dfe5bbd8c842ab511064182a4dd8d948b8371
Maslov Rustislav <maslov.rustislav@gmail.com>
2024-03-04 21:33:19 +0200

    solved top K frequent number problem

--- topKFrequent/index.js
+++ topKFrequent/index.js
@@ -3,4 +3,29 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function (nums, k) {};
+var topKFrequent = function (nums, k) {
+  const result = [];
+  const temp = [];
+  const numsHash = new Map();
+
+  nums.forEach((num) => {
+    const hashItem = numsHash.get(num);
+    if (hashItem) {
+      numsHash.set(num, hashItem + 1);
+    } else {
+      numsHash.set(num, 1);
+    }
+  });
+  for (const [key, val] of numsHash) {
+    temp.push({ key, val });
+  }
+
+  temp.sort((a, b) => {
+    return b.val - a.val;
+  });
+  for (let i = 0; i < k; i++) {
+    result.push(temp[i].key);
+  }
+
+  return result;
+};
